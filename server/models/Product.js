5fa92f67520d630336170e50f0812d0d816546db
Yota Chatzi <yotachatzi@gmail.com>
2020-04-13 22:19:09 +0200

    add ProductSchema

---
+++ server/models/Product.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Products', ProductSchema);
