547c8dbf9f7971db4493405c48a1c6056e96be0b
Yota Chatzi <yotachatzi@gmail.com>
2020-04-13 21:59:58 +0200

    add database configuration & dependencies

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
+const app = express();
 const port = 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+function Console(text) {
+  console.log(text);
+}
+
+mongoose.connect(
+  process.env.DATABASE_CREDENTIALS,
+  { useNewUrlParser: true },
+  () => Console.log('🎉 Database connected successfully!'),
+);
 
 app.listen(port, () => console.log(`🚀 Server listening on port ${port}. Happy Hacking!`));
